4cbe305e60a90f72716fe8feede17fad51c1742d
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-07-15 01:11:52 -0300

    add anotaçoes

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,22 +1,36 @@
 package models;
 
-import java.io.Serializable;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class User {
-	
+
 	@Id
+	@Column(name = "enrollment")
+	@NotNull(message = "User enrollment can not be null")
+	@NotEmpty(message = "User enrollment can not be empty")
 	private String enrollment;
+
+	@Column(name = "email")
+	@NotNull(message = "User email can not be null")
+	@NotEmpty(message = "User email can not be empty")
 	private String email;
+
+	@Column(name = "name")
+	@NotNull(message = "User name can not be null")
+	@NotEmpty(message = "User name can not be empty")
 	private String name;
+
+	@Column(name = "password")
+	@NotNull(message = "User password can not be null")
+	@NotEmpty(message = "User password can not be empty")
 	private String password;
 
 	public User() {
