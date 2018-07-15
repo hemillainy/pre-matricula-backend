90947eb09d63bc446f690a04d3158cb364e3cdf8
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-07-15 01:05:04 -0300

    removido serializable e @GenaratedValue

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -6,16 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 @Entity
-public abstract class User implements Serializable {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class User {
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String enrollment;
 	private String email;
 	private String name;
