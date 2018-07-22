32ddc097d1b02b416ece6a9ceec3deb7aa4abeb6
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2018-07-22 14:55:13 -0300

    Refatoracoes

--- src/main/java/com/sistemaprematricula/repositories/UsersRepository.java
+++ src/main/java/com/sistemaprematricula/repositories/UsersRepository.java
@@ -2,11 +2,9 @@ package com.sistemaprematricula.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.sistemaprematricula.models.user.User;
 
-@Repository
 public interface UsersRepository extends JpaRepository<User, String> {
 	
 
