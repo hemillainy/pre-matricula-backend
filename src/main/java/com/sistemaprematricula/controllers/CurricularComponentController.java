b8eb3b7fa8dc4ab69cc4527d75ca38d5a12f85a2
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-07-23 21:02:25 -0300

    adicionar anotaçoes e save()

--- src/main/java/com/sistemaprematricula/controllers/CurricularComponentController.java
+++ src/main/java/com/sistemaprematricula/controllers/CurricularComponentController.java
@@ -1,5 +1,25 @@
 package com.sistemaprematricula.controllers;
 
-public class CurricularComponentController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.sistemaprematricula.models.CurricularComponent;
+import com.sistemaprematricula.services.CurricularComponentService;
+
+@RestController
+@RequestMapping(value="/curricularComponent")
+@CrossOrigin(value="*")
+public class CurricularComponentController {
+	
+	@Autowired
+	CurricularComponentService curricularComponentService;
+	
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	public CurricularComponent save(@RequestBody CurricularComponent curricularComponent) {
+		return this.curricularComponentService.save(curricularComponent);
+	}
 }
