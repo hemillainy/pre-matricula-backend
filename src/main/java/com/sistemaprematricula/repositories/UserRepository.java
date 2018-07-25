4a8c3dbce42de09817a04070ab937b80bc3345f8
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2018-07-25 20:25:22 -0300

    Adicinar notação @Repository em userRepository

--- src/main/java/com/sistemaprematricula/repositories/UserRepository.java
+++ src/main/java/com/sistemaprematricula/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.sistemaprematricula.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sistemaprematricula.models.user.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 	
 
