d79cc26caff3ff3928d976b0348bc459b7134b24
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-07-30 19:56:32 -0300

    adicionar password em Coodinator

--- src/main/java/com/sistemaprematricula/models/user/User.java
+++ src/main/java/com/sistemaprematricula/models/user/User.java
@@ -61,12 +61,4 @@ public class User {
 		this.name = name;
 	}
 
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
 }
